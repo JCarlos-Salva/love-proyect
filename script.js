8cc04e563567d2415c00cdd2e77fa84c2d0a36ba
JCarlos-Salva <jcarlos3196x@gmail.com>
2025-05-05 21:53:16 -0500

    2.0

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const startDate = new Date('2024-05-03T00:00:00');
+const startDate = new Date('2024-07-31T19:00:00');
 const contador = document.getElementById('contador');
 
 function actualizarContador() {
